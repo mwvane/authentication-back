d5e0bafd118a54b60c7a4e2edc67ade9a2daf4e2
mwvane <bzishvili57@gmail.com>
2023-01-11 01:28:56 +0400

    Create StorageHelper

---
+++ authentication-back/StorageHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace authentication_back
+{
+    public static  class StorageHelper
+    {
+        public static string defaultPath = Directory.GetCurrentDirectory() + "\\users.json";
+        public static void Save(List<User> users)
+        {
+            var jsonFormmated = JsonConvert.SerializeObject(users);
+            Console.WriteLine(jsonFormmated);
+            File.WriteAllText(defaultPath, jsonFormmated);
+
+        }
+        public static List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            if (File.Exists(defaultPath))
+            {
+                string justText = File.ReadAllText(defaultPath);
+                List<User> u = JsonConvert.DeserializeObject<List<User>>(justText);
+                Console.WriteLine(u);
+                return u;
+            }
+            return null;
+        }
+    }
+}
